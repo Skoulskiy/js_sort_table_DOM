3dd7b22600215b12186a2f0b7d10e4c45ed5b5d5
Skoulskiy <ogurethcs@gmail.com>
2026-07-06 11:21:15 +0300

    added solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,29 @@
 'use strict';
 
 // write code here
+const headers = document.querySelector('thead');
+
+headers.querySelectorAll('th').forEach((header, index) => {
+  header.addEventListener('click', () => {
+    const table = header.closest('table');
+    const tbody = table.querySelector('tbody');
+    const rows = Array.from(tbody.querySelectorAll('tr'));
+    sortTableByHeader(table, header.textContent.toLowerCase());
+  });
+})
+
+function sortTableByHeader(table, name) {
+  const tbody = table.querySelector('tbody');
+  if(name === 'name') {
+    tbody.append(...Array.from(tbody.querySelectorAll('tr')).sort((a, b) => a.cells[0].textContent.localeCompare(b.cells[0].textContent)));
+  }
+  if(name === 'position') {
+    tbody.append(...Array.from(tbody.querySelectorAll('tr')).sort((a, b) => a.cells[1].textContent.localeCompare(b.cells[1].textContent)));
+  }
+  if(name === 'age') {
+    tbody.append(...Array.from(tbody.querySelectorAll('tr')).sort((a, b) => a.cells[2].textContent - b.cells[2].textContent));
+  }
+  if(name === 'salary') {
+    tbody.append(...Array.from(tbody.querySelectorAll('tr')).sort((a, b) => Number(a.cells[3].textContent.replace(/[^0-9.-]+/g,"")) - Number(b.cells[3].textContent.replace(/[^0-9.-]+/g,""))));
+  }
+}
