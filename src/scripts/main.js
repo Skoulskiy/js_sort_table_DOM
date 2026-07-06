df103dea3d393afb3d745cf738f86eef7412d2b6
Skoulskiy <ogurethcs@gmail.com>
2026-07-06 11:23:28 +0300

    added fixes

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,24 +6,54 @@ const headers = document.querySelector('thead');
 headers.querySelectorAll('th').forEach((header, index) => {
   header.addEventListener('click', () => {
     const table = header.closest('table');
-    const tbody = table.querySelector('tbody');
-    const rows = Array.from(tbody.querySelectorAll('tr'));
+
+    header.classList.toggle('sorted-asc');
     sortTableByHeader(table, header.textContent.toLowerCase());
   });
-})
+});
 
-function sortTableByHeader(table, name) {
+function sortTableByHeader(table, rowName) {
   const tbody = table.querySelector('tbody');
-  if(name === 'name') {
-    tbody.append(...Array.from(tbody.querySelectorAll('tr')).sort((a, b) => a.cells[0].textContent.localeCompare(b.cells[0].textContent)));
+
+  if (rowName === 'name') {
+    const arrayRows = Array.from(tbody.querySelectorAll('tr'));
+
+    tbody.append(
+      ...arrayRows.sort((a, b) => {
+        return a.cells[0].textContent.localeCompare(b.cells[0].textContent);
+      }),
+    );
   }
-  if(name === 'position') {
-    tbody.append(...Array.from(tbody.querySelectorAll('tr')).sort((a, b) => a.cells[1].textContent.localeCompare(b.cells[1].textContent)));
+
+  if (rowName === 'position') {
+    const arrayRows = Array.from(tbody.querySelectorAll('tr'));
+
+    tbody.append(
+      ...arrayRows.sort((a, b) => {
+        return a.cells[1].textContent.localeCompare(b.cells[1].textContent);
+      }),
+    );
   }
-  if(name === 'age') {
-    tbody.append(...Array.from(tbody.querySelectorAll('tr')).sort((a, b) => a.cells[2].textContent - b.cells[2].textContent));
+
+  if (rowName === 'age') {
+    const arrayRows = Array.from(tbody.querySelectorAll('tr'));
+
+    tbody.append(
+      ...arrayRows.sort(
+        (a, b) => a.cells[2].textContent - b.cells[2].textContent,
+      ),
+    );
   }
-  if(name === 'salary') {
-    tbody.append(...Array.from(tbody.querySelectorAll('tr')).sort((a, b) => Number(a.cells[3].textContent.replace(/[^0-9.-]+/g,"")) - Number(b.cells[3].textContent.replace(/[^0-9.-]+/g,""))));
+
+  if (rowName === 'salary') {
+    const arrayRows = Array.from(tbody.querySelectorAll('tr'));
+
+    tbody.append(
+      ...arrayRows.sort(
+        (a, b) =>
+          Number(a.cells[3].textContent.replace(/[^0-9.-]+/g, '')) -
+          Number(b.cells[3].textContent.replace(/[^0-9.-]+/g, '')),
+      ),
+    );
   }
 }
